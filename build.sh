f29154d740b9963b3d3e54cb84695c70354d2444
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-03-02 08:29:10 +0700

    build.sh: Major changes.
    * Add variable, provide explanations on each step and others.

--- build.sh
+++ build.sh
@@ -1,16 +1,40 @@
-# sync rom
-repo init -u  -b t13.0 https://github.com/AICP/platform_manifest.git --git-lfs --depth=1
-git clone https://github.com/zaidanprjkt/local_manifest.git --depth 1 -b aicp .repo/local_manifests
+# Simple Script for building ROM, Especially Crave.
+# Copyright (C) 2023 newestzdn
+
+# Define variable 
+branch_tree=aicp
+branch_rom=t13.0
+rom_manifest=https://github.com/AICP/platform_manifest.git
+build_command=make bacon
+
+# Do repo init for rom that we want to build.
+repo init -u --no-repo-verify -b $branch_rom $rom_manifest --git-lfs --depth=1
+
+# Remove tree before cloning our manifest.
+rm -rf device/xiaomi
+rm -rf vendor/xiaomi
+rm -rf kernel/xiaomi
+
+# Clone our local manifest.
+git clone https://github.com/zaidanprjkt/local_manifest.git --depth 1 -b $branch_tree .repo/local_manifests
+
+# Do remove here before repo sync.
 rm -rf prebuilts/clang/host/linux-x86
-#external/chromium-webview
+
+# Let's sync!
 repo sync -c -j4 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
     
-# build rom  
+# Do lunch
 source build/envsetup.sh 
 lunch aicp_chime-userdebug
+
+# Define build username and hostname things, also kernel
 export BUILD_USERNAME=zaidan
 export BUILD_HOSTNAME=crave       
 export KBUILD_BUILD_USER=zaidan    
 export KBUILD_BUILD_HOST=authority
-export TZ=Asia/Jakarta 
-make bacon -j$(nproc --all)
+# Define timezone
+export TZ=Asia/Jakarta
+
+# Let's start build!
+$build_command -j$(nproc --all)
