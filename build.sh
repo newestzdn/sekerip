64f3a198a543bbae6cf5b8d6064a2e0ae8fd1b16
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-03-09 06:12:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,6 +76,9 @@ git clone -b arrow-13.1 --depth=1 https://github.com/ArrowOS/android_vendor_qcom
 rm -rf packages/resources/devicesettings
 git clone -b lineage-20.0 https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
 
+rm -rf hardware/xiaomi
+git clone -b lineage-20.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+
 # Do lunch
 source build/envsetup.sh 
 lunch "${rom_name}"_"${device_codename}"-userdebug
