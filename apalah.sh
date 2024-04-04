03efa7837b8608e9628f24a0544346a8f53048d7
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-04-04 18:42:46 +0700

    Update apalah.sh

--- apalah.sh
+++ apalah.sh
@@ -7,12 +7,12 @@
 do_cleanremove=no
 
 # Do repo init for rom that we want to build.
-repo init --depth=1 -b 11 https://github.com/CorvusOS-Revived/android_manifest.git --git-lfs --no-repo-verify
+repo init --depth=1 -u https://github.com/BananaDroid-Revived/android_manifest.git -b 11 --git-lfs --no-repo-verify
 
 # Remove tree before cloning our manifest.
 rm -rf device vendor kernel packages/apps/Settings frameworks/base
 
-git clone -b corvus https://github.com/zaidanprjkt/local_manifest .repo/local_manifests
+git clone -b banana https://github.com/zaidanprjkt/local_manifest .repo/local_manifests
 
 # Do remove here before repo sync.
 if [ "$do_cleanremove" = "yes" ]; then
@@ -28,13 +28,10 @@ fi
 
 # Do lunch
 . build/envsetup.sh
-lunch corvus_juice-user
+lunch banana_juice-user
 
 rm -rf packages/apps/Settings
-git clone --depth=1 -b patch-1 https://github.com/newestzdn/android_packages_apps_Settings-2 packages/apps/Settings
-
-rm -rf frameworks/base
-git clone --depth=1 -b 11 https://github.com/newestzdn/fwb_corvus frameworks/base
+git clone --depth=1 -b 11 https://github.com/newestzdn/settings_banana packages/apps/Settings
 
 # Define build username and hostname things, also kernel
 export BUILD_USERNAME=zaidan
@@ -45,4 +42,4 @@ export KBUILD_BUILD_HOST=authority
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 # Let's start build!
-mka corvus -j$(nproc --all)
+make banana -j$(nproc --all)
