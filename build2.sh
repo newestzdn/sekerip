e622520ce88ac5d721cf2b58e07f3a7c6dbff2c5
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-05-16 06:36:54 +0700

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -7,7 +7,7 @@
 do_cleanremove=no
 
 # Do repo init for rom that we want to build.
-repo init --depth=1 -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --no-repo-verify
+repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b uqpr2 --git-lfs --no-repo-verify
 
 # Remove tree before cloning our manifest.
 rm -rf device vendor kernel hardware prebuilts platfrom
@@ -31,10 +31,10 @@ git clone --depth=1 -b fourteen https://github.com/zaidanprjkt/vendor_xiaomi_lim
 git clone --depth=1 -b u https://github.com/zaidanprjkt/vendor_xiaomi_citrus vendor/xiaomi/citrus
 git clone --depth=1 -b ursinia https://github.com/liliumproject/kernel_xiaomi_chime kernel/xiaomi/sm6115
 
-git clone --depth=1 -b rising https://github.com/zaidanprjkt/device_xiaomi_lime device/xiaomi/lime
-git clone --depth=1 -b rising https://github.com/zaidanprjkt/device_xiaomi_citrus device/xiaomi/citrus
+git clone --depth=1 -b cherish https://github.com/zaidanprjkt/device_xiaomi_lime device/xiaomi/lime
+git clone --depth=1 -b cherish https://github.com/zaidanprjkt/device_xiaomi_citrus device/xiaomi/citrus
 
-git clone --depth=1 https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi
+#git clone --depth=1 https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi
 
 # Do lunch
 . build/envsetup.sh
@@ -50,11 +50,9 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_INCORRECT_PARTITION_IMAGES=true
 #export TARGET_DEFAULT_PIXEL_LAUNCHER=true 
 
-riseup lime userdebug
-rise b
+. build/envsetup.sh
+brunch lime
 
-riseup citrus userdebug
-rise b
-
+brunch citrus
 
 
