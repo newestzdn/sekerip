7b2e7aefa7b51209524ffc2a39ee07e700292a84
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-05-24 07:29:10 +0700

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -10,7 +10,7 @@ do_cleanremove=no
 repo init --depth=1 -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --no-repo-verify
 
 # Remove tree before cloning our manifest.
-rm -rf device/xiaomi vendor/xiaomi kernel/xiaomi hardware/xiaomi platfrom
+rm -rf device/xiaomi vendor/xiaomi kernel/xiaomi hardware/xiaomi external/chromium-webview
 #packages/apps/Settings frameworks/base 
 
 # Let's sync!
