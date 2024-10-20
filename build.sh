6316c5a1a4e6fdf07dfebba93fc0d963154a4256
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-10-20 12:54:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,68 +4,13 @@
 #rm -rf .repo
 
 # Define variable 
-device_codename=chime
-rom_name=lineage
-build_type=userdebug
-do_cleanremove=no
+device_codename=juice
+rom_name=carbon
+build_type=user
 
-if [ "$rom_name" = "aicp" ]; then
-  rom_manifest="https://github.com/AICP/platform_manifest.git"
-  branch_rom="t13.0"
-  branch_tree="aicp"
-  build_command="make bacon"
-  version_android="lineage-20.0"
-fi
-
-if [ "$rom_name" = "carbon" ]; then
-  rom_manifest="https://github.com/CarbonROM/android.git"
-  branch_rom="cr-11.0"
-  branch_tree="carbon13"
-  build_command="make carbon"
-  version_android="lineage-20.0"
-fi
-
-if [ "$rom_name" = "afterlife" ]; then
-  rom_manifest="https://github.com/AfterLifePrjkt13/android_manifest"
-  branch_rom="LTS"
-  branch_tree="afterlife"
-  build_command="m afterlife"
-  version_android="lineage-20.0"
-fi
-
-if [ "$rom_name" = "plros" ]; then
-  rom_manifest="https://github.com/plros/manifests.git"
-  branch_rom="lineage-20.0"
-  branch_tree="plros"
-  build_command="m bacon"
-fi
-
-if [ "$rom_name" = "lmodroid" ]; then
-  rom_manifest="https://github.com/burhancodes/lmodroid"
-  branch_rom="thirteen"
-  branch_tree="lmodroid"
-  build_command="m bacon"
-  version_android="lineage-20.0"
-fi
-
-if [ "$rom_name" = "aosp" ]; then
-  rom_manifest="https://github.com/newestzdn/manifest"
-  branch_rom="aosp-13"
-  branch_tree="void"
-  build_command="mka bacon"
-  version_android="lineage-20.0"
-fi
-
-if [ "$rom_name" = "lineage" ]; then
-  rom_manifest="https://github.com/LineageOS/android.git"
-  branch_rom="lineage-21.0"
-  branch_tree="lineage"
-  build_command="m bacon"
-  #version_android="lineage-20.0"
-fi
 
 # Do repo init for rom that we want to build.
-repo init -u "${rom_manifest}" -b "${branch_rom}"  --git-lfs --depth=1 --no-repo-verify
+repo init -u https://github.com/CarbonROM/android -b cr-9.0  --git-lfs --depth=1 --no-repo-verify
 
 # Remove tree before cloning our manifest.
 rm -rf device vendor kernel packages/resources/devicesettings system/core 
@@ -104,7 +49,7 @@ git clone https://github.com/zaidanprjkt/local_manifest.git --depth 1 -b $branch
 lunch "${rom_name}"_"${device_codename}"-ap1a-userdebug
 
 # Allow neverallow if userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+#xport SELINUX_IGNORE_NEVERALLOWS=true
 
 # Define build username and hostname things, also kernel
 export BUILD_USERNAME=zaidan
